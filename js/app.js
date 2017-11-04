f2493a558139629f810e6a6b6999b4f889267ca6
qibite <qibite@mail.ru>
2017-11-05 07:11:39 +0900

    HTML is ready

--- js/app.js
+++ js/app.js
@@ -5,20 +5,28 @@ jQuery(document).ready(($)=>{
 		$('#podzagolovok').removeClass('align-self-end').addClass('align-self-start')
 		$('#contaner_zagolovok').removeClass('row')
 	}
+	let top_1 = $('#anim_im_1').offset().top;
+	let top_2 = $('#anim_im_2').offset().top;
+	let top_3 = $('#anim_im_3').offset().top;
+	let top_4 = $('#anim_im_4').offset().top;
+	let top_5 = $('#anim_im_5').offset().top;
+	let top_6 = $('#anim_im_6').offset().top;
+	let top_7 = $('#anim_im_7').offset().top;
 	$(window).scroll(()=>{
 		let top;
 		$('#header').css('background-position-y', -($(this).scrollTop()/7)+'px');
-		//console.log($(this).scrollTop())
 		$('#contaner_zagolovok').css('top', -($(this).scrollTop()/3)+'px');
 		let first = document.getElementById('first');
-		//console.log(first.offsetTop)
-		if (first.offsetTop / $(this).scrollTop() < 2) {			
-			$('.whyimg, #txt_1').css('display', 'inline-block');
-			$('#anim_im_1').addClass('bounceInLeft');
-			$('#anim_im_2').addClass('bounceIn');
-			$('#anim_im_3').addClass('bounceInRight');
-		}
+		if ((top_1-250) < $(this).scrollTop()) {$('#anim_im_1').addClass('bounceInLeft').css('visibility', 'visible')}
+		if ((top_2-250) < $(this).scrollTop()) {$('#anim_im_2').addClass('bounceIn').css('visibility', 'visible')}
+		if ((top_3-250) < $(this).scrollTop()) {$('#anim_im_3').addClass('bounceInRight').css('visibility', 'visible')}
+		if ((top_4-250) < $(this).scrollTop()) {$('#anim_im_4').addClass('bounceInLeft').css('visibility', 'visible')}
+		if ((top_5-250) < $(this).scrollTop()) {$('#anim_im_5').addClass('bounceIn').css('visibility', 'visible')}
+		if ((top_6-250) < $(this).scrollTop()) {$('#anim_im_6').addClass('bounceInRight').css('visibility', 'visible')}
+		if ((top_7-250) < $(this).scrollTop()) {$('#anim_im_7').addClass('bounceInLeft').css('visibility', 'visible')}
 	})
 	//
+	$("#phone").mask("+7 (999) 999-99-99");
+
 })
 
